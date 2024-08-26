a3af7880f92f94f672c8d890663c278ae99a5ac1
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-08-26 17:52:51 +0530

    modified code

--- nodejs/files/myserver.js
+++ nodejs/files/myserver.js
@@ -1,14 +1,12 @@
-const http = require('http')
-const { url } = require('inspector')
-const port = 6851
-//to handle the req and responce in server here implemented requestHandler function first function
+const http = require('http') 
+const port = 3000
 
 const requestHandler =((req, res)=>{
     console.log(req.url)
     res.end("hello node js")
 });
 
-//creating server
+
 const server = http.createServer(requestHandler)
 server.listen(port,(err)=>{
     if(err)
