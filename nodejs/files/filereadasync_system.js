357b507872f7734301d73c2a376a43969733c51e
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-09-28 22:46:32 +0530

    updated

---
+++ nodejs/files/filereadasync_system.js
@@ -0,0 +1,25 @@
+
+//creating file
+
+//fs.readFile = which is used for reading file on computer
+
+
+// const http = require('http');
+// const fs = require('fs');
+// const { error } = require('console');
+// http.createServer((request,response)=>{
+//         response.writeHead(200,{
+//             'Content-Type' : 'text/html'
+//         });
+//         fs.readFile('index.html','utf-8',(err,data)=>{
+//             if(err){
+//                 response.write("<h1 style='color:red;'>404 Error: Page Not Found</h1>");
+//             }
+//             else{
+//                 response.write(data);
+//                 //console.log(data);
+//             }
+//         });
+//         (()=>response.write("<h1>Fill the form </h1>"))();
+
+// }).listen(3000);
