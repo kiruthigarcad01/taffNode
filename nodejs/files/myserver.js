ce7b4b456c23b9e7d0b2134a1061634b94ce94d9
kiruthigarcad01 <kiruthigarcad01@gmail.com>
2024-09-28 22:25:51 +0530

    added

--- nodejs/files/myserver.js
+++ nodejs/files/myserver.js
@@ -1,18 +1,41 @@
-const http = require('http') 
-const port = 3000
+// const http = require('http') 
+// const port = 3000
 
-const requestHandler =((req, res)=>{
-    console.log(req.url)
-    res.end("hello node js")
-});
+// const requestHandler =((req, res)=>{
+//     console.log(req.url)
+//     res.end("hello node js")
+// });
 
 
-const server = http.createServer(requestHandler)
-server.listen(port,(err)=>{
-    if(err)
-    {
-        return console.log("Something is happens")
-    }
-    console.log(`server is running on local port ${port}`)
-});
+// const server = http.createServer(requestHandler)
+// server.listen(port,(err)=>{
+//     if(err)
+//     {
+//         return console.log("Something is happens")
+//     }
+//     console.log(`server is running on local port ${port}`)
+// });
 
+//API Server by using express
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+
+// app.get('/user',(req,res)=>{
+//     console.log(req.url);
+//     res.send("Hello ...");
+// });
+
+
+// app.listen(port,(err)=>{
+
+//     if(err)
+//         {
+//             return console.log("Something is happens")
+//         }
+//         console.log(`server is running on local port ${port}`)
+    
+
+// });
--- nodejs/files/server.js
+++ nodejs/files/server.js
@@ -1,9 +1,25 @@
 //am going to creating a server by using http module in node.js (built -in modules)
 
-const http = require('http');
-http.createServer(function(req,res){
-res.write("Hey kiruthiga,Hello world");
-res.end();
-}).listen(3000);
+// const http = require('http');
+// http.createServer(function(req,res){
+// res.write("Hey kiruthiga,Hello world");
+// res.end();
+// }).listen(3000);
+
+const fs = require('fs');
+function readFile(callback) {
+    fs.readFile('text.txt', 'utf8', (err, data) => {
+        if (err) throw err
+        callback(data);
+    });
+}
+function displayContent(content) {
+    console.log(content);
+}
+
+
+readFile(displayContent);
+
+
 
 
